1a93d5a3f114d94f93621f098d0908bb81c35c68
laurasierra17 <laura.sierra17@gmail.com>
2022-08-22 20:34:07 -0400

    click event handler on the install element

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -7,16 +7,14 @@ const butInstall = document.getElementById('buttonInstall');
 window.addEventListener('beforeinstallprompt', (event) => {
     event.preventDefault();
     butInstall.style.visibility = 'visible';
-
-    butInstall.addEventListener('click', () => {
-        event.prompt();
-        butInstall.setAttribute('disabled', true);
-        butInstall.textContent = 'Installed!';
-    });
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => { });
+butInstall.addEventListener('click', async (event) => {
+    event.prompt();
+    butInstall.setAttribute('disabled', true);
+    butInstall.textContent = 'Installed!';
+});
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => { });
